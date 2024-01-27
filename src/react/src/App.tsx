316c56f09b858133b11437e725fb6e40244ed0a5
KubrickCode <126654223+KubrickCode@users.noreply.github.com>
2024-01-27 13:50:45 +0000

    React Nest Rest API 연결
    - axios 설치
    - 리액트쪽에서 proxy 설정
    - get 요청 콘솔 출력

--- src/react/src/App.tsx
+++ src/react/src/App.tsx
@@ -1,9 +1,15 @@
 import { ChakraProvider, Button } from "@chakra-ui/react";
+import axios from "axios";
 
 export const App = () => {
+  const getData = async () => {
+    const result = await axios.get("/api");
+    console.log(result.data);
+  };
+
   return (
     <ChakraProvider>
-      <Button>Chakra Button</Button>
+      <Button onClick={getData}>Chakra Button</Button>
     </ChakraProvider>
   );
 };
