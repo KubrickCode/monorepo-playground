72f88a713e876671a3b0c4b9082207d1554d0f03
KubrickCode <126654223+KubrickCode@users.noreply.github.com>
2024-01-27 04:18:23 +0000

    react, nest 포트 변경

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,5 +3,8 @@
 # just 설치
 curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to /usr/local/bin/
 
+# nc, parallel 설치
+apt-get update && apt-get install -y netcat-openbsd parallel
+
 # Yarn 최신 버전 사용
 yarn set version berry
