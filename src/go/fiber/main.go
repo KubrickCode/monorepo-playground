8ca3d694aaff799c9f1316de17a091b5ee5adfd2
KubrickCode <126654223+KubrickCode@users.noreply.github.com>
2024-01-28 13:00:14 +0000

    fiber rest api 라우터 분리

--- src/go/fiber/main.go
+++ src/go/fiber/main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"db"
-	"strconv"
+	userRouter "fiber-server/user"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -16,75 +15,7 @@ func main() {
 
     api := app.Group("/go")
 
-    userAPI := api.Group("/users")
- 
-    userAPI.Get("", func(c *fiber.Ctx) error {
-        users, err := db.GetUsers()
-        if err != nil {
-            return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-                "error": "Could not fetch users",
-            })
-        }
-
-        return c.JSON(users)
-    })
-
-    userAPI.Post("", func(c *fiber.Ctx) error {
-        var body struct {
-            Name string `json:"name"`
-        }
-
-        if err := c.BodyParser(&body); err != nil {
-            return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-                "error": "Could not parse JSON",
-            })
-        }
-
-        user, err := db.CreateUser(body.Name)
-        if err != nil {
-            return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-                "error": "Could not create user",
-            })
-        }
-
-        return c.JSON(user)
-    })
-
-    userAPI.Put("/:id", func(c *fiber.Ctx) error {
-        var body struct {
-            Name string `json:"name"`
-        }
-
-        if err := c.BodyParser(&body); err != nil {
-            return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-                "error": "Could not parse JSON",
-            })
-        }
-
-        id, _ := strconv.Atoi(c.Params("id", ""))
-
-        user, err := db.UpdateUser(id, body.Name)
-        if err != nil {
-            return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-                "error": "Could not update user",
-            })
-        }
-
-        return c.JSON(user)
-    })
-
-    userAPI.Delete("/:id", func(c *fiber.Ctx) error {
-        id, _ := strconv.Atoi(c.Params("id", ""))
-
-        err := db.DeleteUser(id)
-        if err != nil {
-            return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-                "error": "Could not delete user",
-            })
-        }
-
-        return c.SendStatus(fiber.StatusNoContent)
-    })
+    userRouter.SetupUserRoutes(api)
  
     app.Listen(":3002")
 }
---
+++ src/go/fiber/user/user.router.go
@@ -0,0 +1,80 @@
+package userRouter
+
+import (
+	"db"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupUserRoutes(api fiber.Router){
+	userAPI := api.Group("/users")
+ 
+    userAPI.Get("", func(c *fiber.Ctx) error {
+        users, err := db.GetUsers()
+        if err != nil {
+            return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+                "error": "Could not fetch users",
+            })
+        }
+
+        return c.JSON(users)
+    })
+
+    userAPI.Post("", func(c *fiber.Ctx) error {
+        var body struct {
+            Name string `json:"name"`
+        }
+
+        if err := c.BodyParser(&body); err != nil {
+            return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+                "error": "Could not parse JSON",
+            })
+        }
+
+        user, err := db.CreateUser(body.Name)
+        if err != nil {
+            return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+                "error": "Could not create user",
+            })
+        }
+
+        return c.JSON(user)
+    })
+
+    userAPI.Put("/:id", func(c *fiber.Ctx) error {
+        var body struct {
+            Name string `json:"name"`
+        }
+
+        if err := c.BodyParser(&body); err != nil {
+            return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+                "error": "Could not parse JSON",
+            })
+        }
+
+        id, _ := strconv.Atoi(c.Params("id", ""))
+
+        user, err := db.UpdateUser(id, body.Name)
+        if err != nil {
+            return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+                "error": "Could not update user",
+            })
+        }
+
+        return c.JSON(user)
+    })
+
+    userAPI.Delete("/:id", func(c *fiber.Ctx) error {
+        id, _ := strconv.Atoi(c.Params("id", ""))
+
+        err := db.DeleteUser(id)
+        if err != nil {
+            return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+                "error": "Could not delete user",
+            })
+        }
+
+        return c.SendStatus(fiber.StatusNoContent)
+    })
+}
