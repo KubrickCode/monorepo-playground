fdcae29b62f3731aaf091e76b4e2a3047cd86986
KubrickCode <126654223+KubrickCode@users.noreply.github.com>
2024-04-13 03:55:18 +0000

    NextJS Form 공용 컴포넌트 생성 및 적용

--- src/next/src/core/form/index.tsx
+++ src/next/src/core/form/index.tsx
@@ -3,5 +3,6 @@ export * from "./field/field";
 export * from "./field/input";
 export * from "./field/label";
 
+export * from "./form";
 export * from "./submit-button";
 export * from "./use-mutation-form";
