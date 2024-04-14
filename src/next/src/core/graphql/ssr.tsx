6c05271c94db835893ac2f28b76beb51c4a8afb4
KubrickCode <126654223+KubrickCode@users.noreply.github.com>
2024-04-14 04:39:37 +0000

    graphql apollo api 설정
    에러 링크 및 defaultOptions 설정

--- src/next/src/core/graphql/ssr.tsx
+++ src/next/src/core/graphql/ssr.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import { ApolloLink, HttpLink } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import {
   ApolloNextAppProvider,
   NextSSRInMemoryCache,
   NextSSRApolloClient,
   SSRMultipartLink,
 } from "@apollo/experimental-nextjs-app-support/ssr";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, path }) => {
+      console.log(`[GraphQL error]: Message: ${message}, Path: ${path}`);
+    });
+  }
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 
 const httpLink = new HttpLink({
   uri: "http://127.0.0.1:3003/graphql",
@@ -16,15 +26,27 @@ const httpLink = new HttpLink({
 const makeClient = () =>
   new NextSSRApolloClient({
     cache: new NextSSRInMemoryCache(),
-    link:
+    link: ApolloLink.from([
+      errorLink,
       typeof window === "undefined"
-        ? ApolloLink.from([
-            new SSRMultipartLink({
-              stripDefer: true,
-            }),
-            httpLink,
-          ])
+        ? new SSRMultipartLink({
+            stripDefer: true,
+          })
         : httpLink,
+    ]),
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: "network-only",
+        errorPolicy: "all",
+      },
+      query: {
+        fetchPolicy: "network-only",
+        errorPolicy: "all",
+      },
+      mutate: {
+        errorPolicy: "all",
+      },
+    },
   });
 
 export const ApolloWrapper = ({ children }: React.PropsWithChildren) => (
