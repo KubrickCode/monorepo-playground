039048c275e9bd346eb1b8816a152933371f2b1b
KubrickCode <126654223+KubrickCode@users.noreply.github.com>
2024-04-17 12:27:37 +0000

    NextJS 세션 정보 화면에 표시

--- src/next/src/app/(auth)/register/components/login-status.tsx
+++ src/next/src/app/(auth)/register/components/login-status.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import { Text } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 
 export const LoginStatus = () => {
   const { data: session } = useSession();
-  console.log(session)
-  return <>123</>;
+  return (
+    <>
+      <Text>유저 정보</Text>
+      <Text>{session?.user?.name}</Text>
+      <Text>{session?.user?.email}</Text>
+      <Text>{session?.user?.image}</Text>
+      <Text>{session?.expires}</Text>
+    </>
+  );
 };
