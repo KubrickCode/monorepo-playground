e90248b46916c8d5a2153898e85859ed95fe7cd7
KubrickCode <126654223+KubrickCode@users.noreply.github.com>
2024-04-21 12:18:04 +0000

    NextJS 세션 만료기한 설정

--- src/next/src/app/api/auth/[...nextauth]/route.ts
+++ src/next/src/app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,9 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  session: {
+    maxAge: 30 * 24 * 60 * 60,
+  },
 });
 
 export { handler as GET, handler as POST };
