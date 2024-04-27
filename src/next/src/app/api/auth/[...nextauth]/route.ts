e5dead0f87b6f7c5ccea66d691fabe3bbc8745c2
KubrickCode <126654223+KubrickCode@users.noreply.github.com>
2024-04-27 05:48:24 +0000

    NextJS 세션 정보 DB에 저장 - prisma adapter

--- src/next/src/app/api/auth/[...nextauth]/route.ts
+++ src/next/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const handler = NextAuth({
   providers: [
@@ -8,8 +12,18 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  adapter: PrismaAdapter(prisma),
   session: {
-    maxAge: 30 * 24 * 60 * 60,
+    maxAge: 30 * 24 * 60 * 60, // 세션 최대 유지 시간
+    strategy: "database", // 데이터베이스에 세션을 저장
+  },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    async session({ session }) {
+      return session;
+    },
   },
 });
 
