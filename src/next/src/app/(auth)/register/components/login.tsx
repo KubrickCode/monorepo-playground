727834ab12fecca4027a29641b3c38fee6fcf538
KubrickCode <126654223+KubrickCode@users.noreply.github.com>
2024-04-28 13:04:06 +0000

    NextJS 로그인 후 Router Push 적용

--- src/next/src/app/(auth)/register/components/login.tsx
+++ src/next/src/app/(auth)/register/components/login.tsx
@@ -2,11 +2,20 @@
 
 import { Button } from "@chakra-ui/react";
 import { signIn, signOut } from "next-auth/react";
+import router from "next/router";
 
 export const Login = () => {
   return (
     <>
-      <Button onClick={() => signIn("google")}>로그인</Button>
+      <Button
+        onClick={() =>
+          signIn("google").then(() => {
+            router.push("/register");
+          })
+        }
+      >
+        로그인
+      </Button>
       <Button onClick={() => signOut()}>로그아웃</Button>
     </>
   );
