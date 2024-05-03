e56842ae060496d1bc5a6fda8ff22783beb2af5a
KubrickCode <126654223+KubrickCode@users.noreply.github.com>
2024-05-03 15:01:18 +0000

    npm-test init 명령어 추가

---
+++ src/npm-test/dist/init.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import fs from "fs";
+import path from "path";
+const defaultConfig = {
+    jsonFilePath: "./data/output.json",
+};
+const configPath = path.join(process.cwd(), "kubrick-config.json");
+fs.writeFile(configPath, JSON.stringify(defaultConfig, null, 2), "utf8", (err) => {
+    if (err) {
+        console.error("Failed to create configuration file:", err);
+        process.exit(1);
+    }
+    console.log(`Configuration file created at ${configPath}`);
+});
