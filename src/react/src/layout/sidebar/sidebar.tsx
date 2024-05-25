7581b405d808df165ff9b08b3fa0852b04043479
KubrickCode <kubrickcode@gmail.com>
2024-05-25 02:02:15 +0000

    sidebar navitem 라우터 돔 link로 변경

--- src/react/src/layout/sidebar/sidebar.tsx
+++ src/react/src/layout/sidebar/sidebar.tsx
@@ -1,13 +1,9 @@
-import { useLocation } from "react-router-dom";
+import { PropsWithChildren } from "react";
+import { Link, useLocation } from "react-router-dom";
+
 import { Flex } from "~/core/layout";
 import { ROUTER_PATH } from "~/core/router";
-import {
-  NavGroup,
-  NavItem,
-  NavItemProps,
-  Sidebar,
-  SidebarSection,
-} from "~/core/sidebar";
+import { NavGroup, NavItem, Sidebar, SidebarSection } from "~/core/sidebar";
 
 export const LayoutSidebar = () => {
   return (
@@ -15,8 +11,8 @@ export const LayoutSidebar = () => {
       <SidebarSection>
         <NavGroup>
           <Flex direction="column" gap={3}>
-            <LayoutNavItem href={ROUTER_PATH.HOME}>Home</LayoutNavItem>
-            <LayoutNavItem href={ROUTER_PATH.DATA_TABLE_TEST}>
+            <LayoutNavItem to={ROUTER_PATH.HOME}>Home</LayoutNavItem>
+            <LayoutNavItem to={ROUTER_PATH.DATA_TABLE_TEST}>
               Data Table Test
             </LayoutNavItem>
           </Flex>
@@ -26,20 +22,24 @@ export const LayoutSidebar = () => {
   );
 };
 
-const LayoutNavItem = ({ href, children }: NavItemProps) => {
+const LayoutNavItem = ({
+  children,
+  to,
+}: PropsWithChildren & { to: string }) => {
   const { pathname } = useLocation();
 
   return (
-    <NavItem
-      bgColor={pathname === href ? "gray.100" : "transparent"}
-      borderRadius={5}
-      display="inline-flex"
-      href={href}
-      paddingX={5}
-      paddingY={2}
-      width="full"
-    >
-      {children}
-    </NavItem>
+    <Link to={to}>
+      <NavItem
+        bgColor={pathname === to ? "gray.100" : "transparent"}
+        borderRadius={5}
+        display="inline-flex"
+        paddingX={5}
+        paddingY={2}
+        width="full"
+      >
+        {children}
+      </NavItem>
+    </Link>
   );
 };
